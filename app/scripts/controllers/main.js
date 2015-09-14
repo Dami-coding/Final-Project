40017426644b0aab6023c81fa54bb9d440d548e9
dami <boldsdee@aol.com>
2015-09-14 19:42:07 +0100

    installed yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swingyAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the swingyAppApp
+ */
+angular.module('swingyAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
